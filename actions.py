6b0d8f4ac53ea3535f24a67351af36776112b485
blindpro <noahbalboa34@gmail.com>
2026-07-03 19:25:11 -0500

    Feat: Add read command to help.

--- actions.py
+++ actions.py
@@ -6,6 +6,7 @@ def load(filename):
 def help():
     print("commands")
     print("help. displays this message")
+    print("read. Reads a key from the .json file.")
 def quit():
         print("quitting...")
         sys.exit()
