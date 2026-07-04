c1b1915fc1e972a6fd951d88f46102753428a97f
blindpro <noahbalboa34@gmail.com>
2026-07-04 01:41:30 -0500

    Feat: Add error handaling.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import parser
+from json import JSONDecodeError
 print("Welcome to the python json inspecter. This is a program that will allow you to inspect a json file and easally navagate threw it.")
 filename = input("Please enter the name of the json file you would like to inspect.")
-parser.load(filename)
+try:
+ parser.load(filename)
+except FileNotFoundError:
+    print("file not found.")
+    parser.parse("quit")
+except JSONDecodeError:
+   print("file is not a valid json file.")
+   parser.parse("quit")
 print("json loaded")
 running = True
 while(running):
